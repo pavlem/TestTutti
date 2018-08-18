f89025c8770c7c1e355acef59f07bcb57a4f1a86
Pavle Mijatovic <paul.mijatovic@gmail.com>
2018-08-18 22:26:39 +0200

    Init commit

---
+++ Tutti/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tutti
+//
+//  Created by Pavle Mijatovic on 8/18/18.
+//  Copyright © 2018 Pavle Mijatovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
