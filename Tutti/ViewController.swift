5e7047d1ec5db38b37a7e42005477fbf79538e08
Pavle Mijatovic <paul.mijatovic@gmail.com>
2018-08-19 10:26:38 +0200

    Test frutti.

--- Tutti/ViewController.swift
+++ Tutti/ViewController.swift
@@ -12,8 +12,48 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-            
-//        requestNextMove(<#T##msg: UnsafeMutablePointer<UnsafeMutablePointer<Msg>?>!##UnsafeMutablePointer<UnsafeMutablePointer<Msg>?>!#>)
+        
+        let point1 = CGPoint(x: 50, y: 50)
+        let point2 = CGPoint(x: 50, y: 60)
+        let point3 = CGPoint(x: 50, y: 70)
+        let point4 = CGPoint(x: 50, y: 80)
+
+        var arr = UnsafeMutablePointer<Msg>.allocate(capacity: 4)
+        arr[0] = Msg(msg: MsgsGoToPoint, point: CGPoint(x: 50, y: 50))
+        arr[1] = Msg(msg: MsgsGoToPoint, point: CGPoint(x: 50, y: 60))
+        arr[2] = Msg(msg: MsgsGoToPoint, point: CGPoint(x: 50, y: 70))
+        arr[3] = Msg(msg: MsgsGoToPoint, point: CGPoint(x: 50, y: 80))
+
+        defer {
+            arr.deinitialize(count: 4)
+            arr.deallocate()
+        }
+        
+//        requestNextMove(&arr.advanced(by: 1))
+//        requestNextMove(&arr)
     }
 }
 
+// MARK: - API methods
+//===================================================================
+//enum MsgAction {
+//    MsgsGoToPoint,
+//    MsgsDrawLineToPoint,
+//    MsgsClearDrawing,
+//    MsgsDoNothing
+//};
+//
+//struct Msg
+//{
+//    enum MsgAction msg;
+//    CGPoint point;
+//};
+//
+///*
+// Please note that this function can block the current thread for some time.
+// It is also not thread save and should be properly garded.
+// */
+//void requestNextMove(struct Msg **msg);
+//
+///* free a message */
+//void freeMsg(struct Msg *msg);
